5542977071027a2dc96869277b0c7e7cd8646b5e
dragomirr <dragomirradovanovic@gmail.com>
2021-09-06 09:10:10 +0200

    Updated plotting script. Updated plotter version.
      - Now it will run endlessly with 10 minutes break between runs.

--- roles/scripts/templates/plot.sh
+++ roles/scripts/templates/plot.sh
@@ -13,8 +13,6 @@ hostname=$(cat /proc/sys/kernel/hostname)
 plotter={{ plotter.directory }}/build/chia_plot
 chia={{ blockchain.directory }}/venv/bin/chia
 
-disks=$(find $plot_disks_directory -mindepth 1 -maxdepth 1 -type d)
-
 finish() {
   curl --header "Content-Type: application/json" -d '{"content": "Plotovanje je stalo na serveru '$hostname'. Potrebno je proveriti server."}' $discord_webhook
 }
@@ -25,17 +23,19 @@ max_plot_number() {
   echo $number_of_plots
 }
 
-for disk in $disks; do
-  plots=$(max_plot_number $disk)
-  if [[ -n $plots ]]; then
-    # add plot directory
-    $chia plots add -d $disk/
-    # run plotting
-    $plotter -t /mnt/ssd/ -r 16 -u 512 -v 256 \
-      -d $disk/ -n $plots \
-      -c {{ contract }} -f {{ farmer_key }}
-  fi
+while :; do
+  disks=$(find $plot_disks_directory -mindepth 1 -maxdepth 1 -type d)
+  for disk in $disks; do
+    plots=$(max_plot_number $disk)
+    if [[ -n $plots ]]; then
+      # add plot directory
+      $chia plots add -d $disk/
+      # run plotting
+      $plotter -t /mnt/ssd/ -r 16 -u 512 -v 256 \
+        -d $disk/ -n $plots \
+        -c {{ contract }} -f {{ farmer_key }}
+    fi
+  done
+  # wait 10 minutes before starting another turn
+  sleep 600
 done
-
-# notify when plotting ends
-curl --header "Content-Type: application/json" -d '{"content": "'$hostname' zavrsio sa plotovanjem"}' $discord_webhook
